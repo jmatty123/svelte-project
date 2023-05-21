3e8c30ef9641690ddc556150dcefba090a58a102
Matthew Young <jmattyoung1@gmail.com>
2023-05-21 15:37:27 -0400

    fix: map response / adjust: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,26 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="pageHeader">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<span class="font-semibold text-xl tracking-tight">matthewyoung.<span style="color: cyan;">x</span><span style="color: yellow;">y</span><span style="color: magenta;">z</span></span>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a href="/" class="px-4 text-white hover:text-gray-900">Home</a>
+				<a href="/about" class="px-4 text-white hover:text-gray-900">About</a>
+				<a href="/services" class="px-4 text-white hover:text-gray-900">Services</a>
+				<a href="/contact" class="px-4 text-white hover:text-gray-900">Contact</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<slot />
+
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,5 @@
 <script>
-	import Map from './Map.svelte';
+	import Bordermap from "./BorderWMap.svelte";
 </script>
-	
-<style>
-	:global(body) {
-	  @apply flex items-center justify-center h-screen m-0 p-0;
-	}
-  
-</style>
-  
-<div id="container" class="container h-screen flex flex-col justify-center items-center m-0 p-0 overflow-hidden">
 
-	<div id="border" class="box-border border-2 border-white rounded-xl flex flex-col justify-between items-center space-y-4 w-4/5 h-4/5">
-  
-	  <p id="content" class="text-center font-serif font-medium text-white pt-4 px-5">
-		This page was built using <a id="svelteLink" class="underline visited:text-gray-300 active:text-gray-500" href="https://svelte.dev/">Svelte</a>
-		 by <a id="linkedinLink" class="underline visited:text-gray-300 active:text-gray-500" href="https://www.linkedin.com/in/jmatty123/">Matthew Young.</a>
-		<br>This project is a learning experience and will be under constant development. Check back regularly!
-	  </p>
-  
-		<div id="mapdiv" class="flex-grow w-full h-1/2 px-5 overflow-hidden">
-		  <Map />
-		</div>
-  
-		<p id="changelog" class="text-center font-mono text-xs text-white pb-4">
-		  Creation: 5/16/23 | Latest update: 5/19/23 "repository/framework adjustments"</p>
-	
-	</div>
-  
-</div>
+<Bordermap />
