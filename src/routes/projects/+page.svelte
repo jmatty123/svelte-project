a1b82506af710779b874fe17d30adbb457527876
Matthew Young <jmattyoung1@gmail.com>
2023-05-26 15:12:30 -0400

    tweaks

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,37 +2,24 @@
 
 </script>
 
-<!-- <div class="w-full h-full flex justify-center items-center p-4"> -->
-    <div class='box-border m-4 pb-4 w-auto h-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-        <a href="/" class="card project-card bg-surface-100-800-token">
-            <div class="content">
-                <p>Personal Website</p>
-                <p class="text-base">5/16/23</p>
-            </div>
-        </a>
-        <a href="/" class="card project-card bg-surface-100-800-token">
-            <div class="content">
-                <p>2</p>
-                <p class="text-base"></p>
-            </div>
-        </a>
-        <a href="/" class="card project-card bg-surface-100-800-token">
-            <div class="content">
-                <p>3</p>
-                <p class="text-base"></p>
-            </div>
-        </a>
-        <a href="/" class="card project-card bg-surface-100-800-token">
-            <div class="content">
-                <p>4</p>
-                <p class="text-base"></p>
-            </div>
-        </a>
-        <a href="/" class="card project-card bg-surface-100-800-token">
-            <div class="content">
-                <p>5</p>
-                <p class="text-base"></p>
-            </div>
-        </a>
+<div class='box-border m-4 pb-4 w-auto h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
+    <a href="/" class="card project-card bg-surface-100-800-token">
+        <div class="content">
+            <p>Personal Website</p>
+            <p class="text-base">5/16/23</p>
+        </div>
+    </a>
+    <a href="/" class="card project-card bg-surface-100-800-token">
+        <div class="content">
+            <p>2</p>
+            <p class="text-base">Coming soon!</p>
+        </div>
+    </a>
+    <a href="/" class="card project-card bg-surface-100-800-token">
+        <div class="content">
+            <p>3</p>
+            <p class="text-base">Coming soon!</p>
+        </div>
+    </a>
+        
     </div>
-<!-- </div> -->
