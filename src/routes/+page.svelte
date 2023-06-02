8dee5dc08ea3e454d4c61bb775d503d67faea102
Matthew Young <jmattyoung1@gmail.com>
2023-06-02 14:25:26 -0400

    threlte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import Map from '../components/Map.svelte';
+	import Threlte from '../components/Threlte.svelte';
 </script>
 
-<div class='h-full flex flex-col justify-center items-center'>
-	<span class='text-9xl opacity-5 font-thin animate-pulse-slow'>M Y</span>
+<div class='h-full w-full flex flex-col justify-center items-center'>
+	<!-- <span class='text-9xl opacity-5 font-thin animate-pulse-slow'>M Y</span> -->
+	<Threlte />
 </div>
