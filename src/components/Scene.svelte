917afaf14d0cf4f98ace175abf698288d2e363fc
Matthew Young <jmattyoung1@gmail.com>
2023-06-02 15:40:08 -0400

    torusknot refining

--- src/components/Scene.svelte
+++ src/components/Scene.svelte
@@ -23,13 +23,13 @@
 <T.DirectionalLight position={[3, 10, 7]} />
 
 <T.Mesh
-    rotation.y={rotation}
-    rotation.x={rotation}
+    rotation.y={rotation/2}
+    rotation.x={rotation/10}
     position.y={1}
     scale={$scale}
     on:pointerenter={() => scale.set(.15)}
     on:pointerleave={() => scale.set(.15)}    
 >
-  <T.TorusKnotGeometry args={[10, 3, 100, 16, 1, 3]} />
+  <T.TorusKnotGeometry args={[10, 3, 1000, 100, 1, 3]} />
   <T.MeshStandardMaterial color="white" />
 </T.Mesh>
