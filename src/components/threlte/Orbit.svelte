7649c72059f1b13a2eb5409c976a7013cab739fc
Matthew Young <jmattyoung1@gmail.com>
2023-06-07 15:41:38 -0400

    orbit x3

--- src/components/threlte/Orbit.svelte
+++ src/components/threlte/Orbit.svelte
@@ -7,20 +7,21 @@
     const scale = spring(.5)
 
     let rotation = 0
-    useFrame((state, delta) => {
-        rotation -= delta/5
-    })
-
     let spherePos1 = { x: 2.25, y: 2.25, z: 0 }
+    let spherePos2 = { x: 2.25, y: 2.25, z: 0 }
+    let spherePos3 = { x: 2.25, y: 2.25, z: 0 }
+
     useFrame((state, delta) => {
+        rotation = (rotation + delta/5) % (2*Math.PI);
+        
         spherePos1.x = 2.25 * Math.cos(rotation);
         spherePos1.y = -2.25 * Math.sin(rotation);
-    })
-    
-    let spherePos2 = { x: 2.25, y: 2.25, z: 0 }
-    useFrame((state, delta) => {
-        spherePos2.x = -2.25 * Math.cos(rotation);
-        spherePos2.y = 2.25 * Math.sin(rotation);
+
+        spherePos2.x = 2.25 * Math.cos(rotation + 2*Math.PI/3);
+        spherePos2.y = -2.25 * Math.sin(rotation + 2*Math.PI/3);
+
+        spherePos3.x = 2.25 * Math.cos(rotation + 4*Math.PI/3);
+        spherePos3.y = -2.25 * Math.sin(rotation + 4*Math.PI/3);
     })
 
 </script>
@@ -53,8 +54,8 @@
     rotation.y={0}
     rotation.z={rotation*2}  
 >
-  <T.IcosahedronGeometry args={[.7, 0]} />
-  <T.MeshStandardMaterial color="red" />
+  <T.IcosahedronBufferGeometry args={[.5, 0]} />
+  <T.MeshStandardMaterial color="cyan" />
 </T.Mesh>
 
 <T.Mesh
@@ -64,6 +65,17 @@
     rotation.y={0}
     rotation.z={rotation*2} 
 >
-  <T.IcosahedronGeometry args={[.7, 0]} />
-  <T.MeshStandardMaterial color="white" />
+  <T.IcosahedronGeometry args={[.5, 0]} />
+  <T.MeshStandardMaterial color="yellow" />
+</T.Mesh>
+
+<T.Mesh
+    position={[spherePos3.x, spherePos3.y, spherePos3.z]}
+    scale={$scale}
+    rotation.x={rotation*2}
+    rotation.y={0}
+    rotation.z={-rotation*2}  
+>
+  <T.IcosahedronBufferGeometry args={[.5, 0]} />
+  <T.MeshStandardMaterial color="magenta" />
 </T.Mesh>
