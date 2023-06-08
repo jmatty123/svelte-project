db76ca782f5c3bd44d4974377086134034032f7a
Matthew Young <jmattyoung1@gmail.com>
2023-06-08 17:48:22 -0400

    tweaks

--- src/routes/projects/threlte-projects/+page.svelte
+++ src/routes/projects/threlte-projects/+page.svelte
@@ -3,30 +3,20 @@
     import TorusKnotSc from '/src/components/threlte/TorusKnotSc.svelte'
 </script>
 
-<div class="box-border border-0 border-red-500 sm:w-[90%] max-w-screen-2xl my-4 sm:my-8 mx-2 sm:mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-8 xl:gap-12">
-    <div class="card pt-[100%] text-center relative flex justify-center items-center">
-        <div class="flex items-center absolute inset-0 mx-2">
+<div class="box-border sm:w-[90%] max-w-screen-2xl my-4 sm:my-8 mx-2 sm:mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-8 xl:gap-12">
+    <div class="card pt-[100%] text-center overflow-clip relative flex justify-center items-center">
+        <div class="flex items-center justify-center absolute inset-0 mx-0">
             <OrbitSc />
         </div>
     </div>
-    <div class="card pt-[100%] text-center relative flex justify-center items-center">
-        <div class="flex items-center absolute inset-0 mx-2">
+    <div class="card pt-[100%] text-center overflow-clip relative flex justify-center items-center">
+        <div class="flex items-center justify-center absolute inset-0 mx-0">
             <TorusKnotSc />
         </div>
     </div>
-    <div class="card pt-[100%] text-center relative flex justify-center items-center">
-        <div class="flex items-center absolute inset-0 mx-2">
-            <OrbitSc />
-        </div>
-    </div>
-    <div class="card pt-[100%] text-center relative flex justify-center items-center">
-        <div class="flex items-center absolute inset-0 mx-2">
-            <OrbitSc />
-        </div>
-    </div>
-    <div class="card pt-[100%] text-center relative flex justify-center items-center">
-        <div class="flex items-center absolute inset-0 mx-2">
-            <OrbitSc />
+    <div class="card pt-[100%] text-center overflow-clip relative flex justify-center items-center">
+        <div class="flex items-center justify-center absolute inset-0 mx-0">
+            <TorusKnotSc />
         </div>
     </div>
 </div>
